8bc834c8e72a65e2f013c4d9558d7b1df4a27625
Minsoo Kim <pos02043@gmail.com>
2023-05-08 22:07:50 +0900

    Add files via upload

---
+++ study1_greedy.py
@@ -0,0 +1,14 @@
+n,m,k = map(int,input().split())
+data = list(map(int,input().split()))
+print(n, m, k )
+data.sort()
+print(data[-1])
+
+first = int(m/k) * k
+second = m%k
+print("first",first)
+print("second",second)
+
+print(data[-1] * first + data[-2] * second)
+
+#print(data[-1]*(k-1))
